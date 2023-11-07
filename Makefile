58453d9297158c404d5a20af6b26ca223630fc39
miggazElquez <miguel.gazquez82@gmail.com>
2023-11-07 18:04:41 +0100

    minimax

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-C_ARGS= 
+C_ARGS= -O3 -g -pg
 
 p4: p4.c
 	gcc p4.c -o p4 $(C_ARGS)
--- p4.c
+++ p4.c
@@ -6,6 +6,8 @@
 #define RED 1
 #define YELLOW 2
 
+#define MAX_DEPTH 7
+
 typedef struct {
 	uint64_t a;
 	uint64_t b;
@@ -24,7 +26,7 @@ int get_val(Board *bo, int col, int row) {
 void set_val(Board *bo, int col, int row, uint64_t val) {
 	uint64_t *temp;
 	uint64_t decal;
-	printf("set %d on %d,%d\n",val,row,col);
+	// printf("set %d on %d,%d\n",val,row,col);
 	if (col >= 4) {
 		temp = &bo->b;
 		decal = (2 * ((col - 4) * 6 + row));
@@ -33,7 +35,7 @@ void set_val(Board *bo, int col, int row, uint64_t val) {
 		decal = (2 * (col * 6 + row));
 	}
 
-	printf("decal = %d\n",decal);
+	// printf("decal = %d\n",decal);
 	uint64_t trois = 4-1;
 	*temp &= ~(trois << decal);
 	*temp |= val << decal;
@@ -57,6 +59,10 @@ void print_board(Board *bo) {
 }
 
 int insert(Board *bo, int col, int color) {
+	if (col > 6) {
+		printf("col trop grande\n");
+		return 1;
+	}
 	int row;
 	for (row =0;row<6;row++) {
 		if (get_val(bo,col,row) == EMPTY) {
@@ -65,42 +71,47 @@ int insert(Board *bo, int col, int color) {
 		}
 	}
 	if (row == 6) {
-		printf("Column %d full\n",col);
+		// printf("Column %d full\n",col);
 		return 1;
 	}
 }
 
 
 //Check probablement lent, à améliorer
-int win_check(Board *bo, int color) {
-	int draw = 2;
+int win_check(Board *bo, int color, int *score) {
+	int res = color==RED?1:-1;
+	int draw = 1;
 	for (int col=0;col<7;col++) {
 		for (int row=0;row<6;row++) {
 			if (get_val(bo,col,row) == color) {
 				if (row < 3) {
 					if (get_val(bo,col,row+1) == color && get_val(bo,col,row+2) == color && get_val(bo,col,row+3) == color) {
-						printf("col, %d %d\n",col,row);
+						// printf("col, %d %d\n",col,row);
+						*score = res;
 						return 1;
 					}
 				}
 
 				if (col < 4) {
 					if (get_val(bo,col+1,row) == color && get_val(bo,col+2,row) == color && get_val(bo,col+3,row) == color){
-						printf("row, %d %d\n",col,row);
-						return 1;				
+						// printf("row, %d %d\n",col,row);
+						*score = res;
+						return 1;
 					}
 				}
 
 				if (col < 4 && row < 3) {
 					if (get_val(bo,col+1,row+1) == color && get_val(bo,col+2,row+2) == color && get_val(bo,col+3,row+3) == color){
-						printf("diag++, %d %d\n",col,row);
-						return 1;					
+						// printf("diag++, %d %d\n",col,row);
+						*score = res;
+						return 1;
 					}
 				}
 				if (col <4 && row > 2) {
 					if (get_val(bo,col+1,row-1) == color && get_val(bo,col+2,row-2) == color && get_val(bo,col+3,row-3) == color) {
-						printf("diag+-, %d %d\n",col,row);
-						return 1;					
+						// printf("diag+-, %d %d\n",col,row);
+						*score = res;
+						return 1;
 					}
 
 				}
@@ -109,10 +120,117 @@ int win_check(Board *bo, int color) {
 			}
 		}
 	}
-	return draw;
+	if (draw) {
+		*score = 0;
+		return 1;
+	}
+	return 0;
 }
 
+int N = 0;
 
+int max(Board *bo,int depth,int alpha);
+
+int min(Board *bo,int depth,int beta) { //Yellow to play
+	int score = 2;
+	// printf("min, depth : %d\n",depth);
+	// print_board(bo);
+
+	N++;
+
+	if (win_check(bo,RED,&score)) {
+		return score;
+	}
+
+	for (int col=0;col<7;col++) {
+		Board temp = *bo;
+		if (insert(&temp,col,YELLOW)) continue;
+		int val = max(&temp,depth+1,score);
+		if (val < score) {
+			score = val;
+			if (score < beta)
+				return score;
+			if (score == -1) {
+				return score;
+			}
+		}
+	}
+
+	// printf("-> min, depth : %d, score : %d\n",depth,score);
+
+	return score;
+
+}
+
+int max(Board *bo,int depth,int alpha) { //Red to play
+	// printf("max, depth : %d\n",depth);
+	// print_board(bo);
+
+	N++;
+
+	if (depth>MAX_DEPTH) return 0;
+	int score = -2;
+
+	if (win_check(bo,YELLOW,&score)) {
+		return score;
+	}
+
+	for (int col=0;col<7;col++) {
+		Board temp = *bo;
+		if (insert(&temp,col,RED)) continue;
+		int val = min(&temp,depth+1,score);
+		if (val > score) {
+			score = val;
+			if (score > alpha)
+				return score;
+
+			if (score == 1)
+				return score;
+		}
+	}
+
+	// printf("-> max, depth : %d, score : %d\n",depth,score);
+	return score;
+}
+
+int cout_coup(Board *bo,int current_color, int* res) {
+	if (current_color == RED) {
+		int score = -2;
+		int coup;
+
+
+		for (int col=0;col<7;col++) {
+			Board temp = *bo;
+			if (insert(&temp,col,RED)) continue;
+			int val = min(&temp,0,score);
+			printf("	%d : %d\n",col,val);
+			if (val > score) {
+				score = val;
+				coup = col;
+			}
+		}
+		*res = score;
+		return coup;
+
+	} else {
+		int score = 2;
+		int coup;
+
+		for (int col=0;col<7;col++) {
+			Board temp = *bo;
+			if (insert(&temp,col,RED)) continue;
+			int val = max(&temp,0,score);
+			printf("	%d : %d\n",col,val);
+			if (val < score) {
+				score = val;
+				coup = col;
+			}
+		}
+		*res = score;
+		return coup;
+
+	}
+}
 
 int main() {
 	uint64_t val = 0x0000000000000000;
@@ -122,22 +240,41 @@ int main() {
 	int current_color = RED;
 
 	print_board(&bo);
+
 	int col;
-	while (1) {
-		scanf("%d",&col);
-		insert(&bo,col,current_color);
-		print_board(&bo);
-		if (win_check(&bo,current_color)) {
-			printf("Bien joué !\n");
-			printf("%lx %lx\n",bo.a,bo.b);
-			exit(0);
-		}
-		if (current_color == RED) {
-			current_color = YELLOW;
-		} else {
-			current_color = RED;
-		}
+	int score;
+	int coup = cout_coup(&bo, RED, &score);
+	printf("%d\n",coup);
+	// while (1) {
+
+	// 	int score;
+
+	// 	int coup = cout_coup(&bo, RED, &score);
+
+	// 	printf("coup : %d, eval=%d\n",coup,score);
+	// 	printf("%d\n",N);
+	// 	N = 0;
+	// 	insert(&bo,coup,RED);
+	// 	print_board(&bo);
 
 
-	}
+	// 	if (win_check(&bo,RED,&score)) {
+	// 		printf("fin de partie !\n");
+	// 		printf("%d\n",score);
+	// 		printf("%lx %lx\n",bo.a,bo.b);
+	// 		exit(0);
+	// 	}
+
+
+	// 	scanf("%d",&col);
+	// 	insert(&bo,col,YELLOW);
+	// 	print_board(&bo);
+
+	// 	if (win_check(&bo,YELLOW,&score)) {
+	// 		printf("fin de partie !\n");
+	// 		printf("%d\n",score);
+	// 		printf("%lx %lx\n",bo.a,bo.b);
+	// 		exit(0);
+	// 	}
+	// }
 }
